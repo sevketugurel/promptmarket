8b99fc3699ed06c3cdf4ce936abfb71eae1b2751
sevketugurel <ugurelsevket@gmail.com>
2026-06-27 13:13:52 +0300

    feat: add TypeScript interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type PromptCategory = 'All' | 'LLMs' | 'Image Generation' | 'Code'
+
+export interface Prompt {
+  id: number
+  title: string
+  description: string
+  category: Exclude<PromptCategory, 'All'>
+  price: number
+  author: string
+  authorHandle: string
+  secretPrompt: string
+  tag: string
+}
+
+export interface PerformanceRecord {
+  promptId: number
+  startMs: number
+  endMs: number | null
+  gasEstimate: string
+}
